a32633a49c2ee160e169d29bd01672851db5cf51
Hiroyuki Yamashita <hiroya@spir.co.jp>
2024-04-12 09:34:57 +0900

    Fixed a bug in pytorch version comparison.

--- slowfast/datasets/multigrid_helper.py
+++ slowfast/datasets/multigrid_helper.py
@@ -6,11 +6,10 @@
 import numpy as np
 import torch
 from torch.utils.data.sampler import Sampler
+from packaging.version import Version
 
-TORCH_MAJOR = int(torch.__version__.split(".")[0])
-TORCH_MINOR = int(torch.__version__.split(".")[1])
 
-if TORCH_MAJOR >= 1 and TORCH_MINOR >= 8:
+if Version('1.8') <= Version(torch.__version__):
     _int_classes = int
 else:
     from torch._six import int_classes as _int_classes
